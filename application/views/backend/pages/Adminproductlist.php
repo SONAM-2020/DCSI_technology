4b9adc91a43014b56c3270af0527897e0f413735
BHUTANSYNC INFOTECH <sdorji815@gmail.com>
2021-08-19 15:07:46 +0600

    adminproduct list updated

--- application/views/backend/pages/Adminproductlist.php
+++ application/views/backend/pages/Adminproductlist.php
@@ -17,7 +17,6 @@
                                 <tr>
                                     <th>No.</th>
                                     <th>Name</th>
-                                    <th style="display: none;">Description</th>
                                     <th>Price</th>
                                     <th>Status</th>
                                     <th>Action</th>
@@ -28,7 +27,6 @@
                                 <tr>
                                     <td><?=$i+1?></td>
                                     <td><?php echo $pro['Product_Name'];?></td>
-                                    <td style="display: none;"><?php echo $pro['Description'];?></td>
                                     <td><?php echo $pro['Price'];?></td>
                                     <td><?php echo $pro['Status'];?></td>
                                     <td>
@@ -44,6 +42,13 @@
         </div>
     </div>
 </main>
+<script src="<?php echo base_url();?>assest/admin/assets/js/jquery.min.js"></script>
+<script src="https://cdn.datatables.net/1.10.23/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/1.6.5/js/dataTables.buttons.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+<script src="https://cdn.datatables.net/buttons/1.6.5/js/buttons.html5.min.js"></script>
 <script type="text/javascript">
      $(document).ready(function() {
     $('#example1').DataTable( {
